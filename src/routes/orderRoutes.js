cc3d543ab44c99254fb72a511d91587e6db3e704
Jimmy9812 <wladimirmaila1998pm@gmail.com>
2025-02-11 21:32:47 -0500

    feat:

---
+++ src/routes/orderRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const orderController = require("../controllers/orderController");
+
+// Ruta para obtener las opciones (datos de usuarios, productos y workshops)
+router.get("/options", orderController.getOptions);
+
+// Ruta para crear una nueva orden
+router.post("/create", orderController.createOrder);
+
+module.exports = router;
